8b1ecec186147a4c6993d2c8c5dc730763ae42d0
Ishz <Xardas366@gmail.com>
2022-07-18 23:43:27 +0300

    Almost Finished
    Almost Finished

---
+++ ASP.NET/NutsShop/NutsShop/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NutsShop.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Desctiption { get; set; }
+        [Range(1,int.MaxValue)]
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+
+    }
+}
